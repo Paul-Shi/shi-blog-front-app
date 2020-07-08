622e22158eac61da9f1af29a6334686d9342ad21
shishunyuan <shishunyuan@iboxpay.com>
2020-07-08 21:22:16 +0800

    首页展示-2

---
+++ src/components/content/ArticleContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="article-content" v-cloak>
+    <iv-row>
+      <iv-col :xs="24" :sm="24" :md="24" :lg="17">
+        <div class="layout-left">
+          <article-page-header :article
+        </div>
+      </iv-col>
+    </iv-row>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ArticleContent"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/header/SimpleHeader/SideBar.vue
+++ src/components/header/SimpleHeader/SideBar.vue
@@ -18,20 +18,113 @@
           </div>
           <ul class="nav-menu">
             <!-- 类别导航 -->
-            <li class=""
+            <li class="nav-dropdown-container" v-for="category_level1 in articleCategoryList" :key="category_level1.id">
+              <iv-icon type="minus-round"></iv-icon>
+              <a class="nav-link" :href="'/articles/category/' + category_level1.id">{{category_level1.name}}<span
+                class="arrow"></span> </a>
+              <ul class="nav-dropdown">
+                <li v-for="category_level2 in category_level1.children" :key="category_level2.id">
+                  <iv-icon type="minus-round"></iv-icon>
+                  <a class="nav-link" :href="'/artocles/category/' + category_level2.id">{{ category_level2.name}}</a>
+                  <ul class="nav-dropdown">
+                    <li v-for="category_level3 in category_level2.children" :key="category_level3.id">
+                      <iv-icon type="minus-round"></iv-icon>
+                      <a class="nav-link" :href="'/articles/category/' + category_level3.id">{{ category_level3.name
+                        }}</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
           </ul>
+        </div>
+        <div class="sidebar-menus">
+          <div class="site-nav">
+            <p>
+              <iv-icon type="map"></iv-icon>
+              阅读导航
+            </p>
+          </div>
+          <ul class="nav-menu">
+            <!-- 类别导航 -->
+            <li class="nav-dropdown-container" v-for="category_level1 in bookCategoryList" :key="category_level1.id">
+              <iv-icon type="minus-round"></iv-icon>
+              <a class="nav-link" :href="'/books/category/' + category_level1.id">{{ category_level1.name }}<span class="arrow"></span></a>
+              <ul class="nav-dropdown">
+                <li v-for="category_level2 in category_level1.children" :key="category_level2.id">
+                  <iv-icon type="minus-round"></iv-icon>
+                  <a class="nav-link" :href="'/books/category/' + category_level2.id" >{{ category_level2.name }}</a>
+                  <ul class="nav-dropdown">
+                    <li v-for="category_level3 in category_level2.children" :key="category_level3.id">
+                      <iv-icon type="minus-round"></iv-icon>
+                      <a class="nav-link" :href="'/books/category/' + category_level3.id">{{ category_level3.name }}</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <div class="sidebar-toc-list" ref="list">
+            <div class="site-nav">
+              <p>
+                <iv-icon type="ios-flower-outline"></iv-icon>
+                文章目录
+              </p>
+            </div>
+            <div id="sidebar-toc" class="list" @click.prevent></div>
+          </div>
         </div>
       </div>
     </div>
+    <div class="mask" @click.prevent="toggleSideBar"></div>
   </div>
 </template>
 
-<script>
-    export default {
-        name: "SideBar"
+<script type="text/ecmascript-6">
+  import {mixin} from "@/utils"
+
+  export default {
+    name: 'side-bar',
+    data() {
+      return {
+        show: false,
+        showNav: false
+      }
+    },
+    props: {
+      articleCategoryList: Array,
+      bookCategoryList: Array
+    },
+    mixins: [mixin],
+    beforeUpdate(to, from, next) {
+      next()
+    },
+    methods: {
+      rootRouterLink(category) {
+        let roouter = {}
+        router.name = category.category_type
+        return roouter
+      },
+      routerLink(category) {
+        let router = {}
+        router.name = category.category_type
+        router.params = {}
+        router.params['id'] = category.id
+        return router
+      },
+      toggleSideBar() {
+        this.show = !this.show
+        this.showNav = !(this.$router.name === 'article' ||
+          this.$router.name === 'book' ||
+          this.$router.name === 'book.note' ||
+          this.$router.name === 'movie' ||
+          this.$router.name === 'album'
+        )
+      }
     }
+  }
 </script>
 
-<style scoped>
-
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+  @import "./stylus/sidebar.styl";
 </style>
