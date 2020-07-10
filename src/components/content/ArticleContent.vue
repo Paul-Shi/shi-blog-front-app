8a1a8096bad1ef527e98ac6ad5357d267cbd6712
shishunyuan <shishunyuan@iboxpay.com>
2020-07-10 21:26:09 +0800

    首页展示-3

--- src/components/content/ArticleContent.vue
+++ src/components/content/ArticleContent.vue
@@ -3,19 +3,42 @@
     <iv-row>
       <iv-col :xs="24" :sm="24" :md="24" :lg="17">
         <div class="layout-left">
-          <article-page-header :article
+          <article-page-header :article="article"></article-page-header>
+          <article-page-content>
+            <article id="article-main-page" class="typo container" slot="content" ref="article" v-html="article.contentFormat">
+            </article>
+          </article-page-content>
+          <article-page-footer :postId="article.id"></article-page-footer>
+        </div>
+      </iv-col>
+      <iv-col :xs="0" :sm="0" :md="0" :lg="7">
+        <div class="layout-right">
+          <recommend></recommend>
+          <iv-affix :offset-top="60">
+            <side-toc style="margin-top: 15px;"></side-toc>
+          </iv-affix>
         </div>
       </iv-col>
     </iv-row>
   </div>
 </template>
 
-<script>
-    export default {
-        name: "ArticleContent"
+<script type="text/ecmascript-6">
+  import ArticlePageHeader from '@/components/views/Article/ArticlePageHeader'
+  import ArticlePageContent from '@/components/views/Article/ArticlePageContent'
+  import ArticlePageFooter from '@/components/views/Article/ArticlePageFooter'
+  import About from '@/components/views/about'
+  import FriendLinks from '@/components/views/FriendLinks'
+  import SideToc from '@/components/views/SideToc'
+  import Recommond from '@/components/views/Recommond'
+  import TOC from '@/common/js/MarkdownToc'
+  import TocScrollSpy from '@/common/js/TocScrollSpy'
+
+  export default {
+    data() {
+      return {
+        article: {}
+      }
     }
+  }
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/components/views/Recommend.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="recommend">
+    <panel :title="'推荐阅读'">
+      <div slot="content" class="content">
+        <div class="top" v-if="topRecommend">
+          <a :href="'/' + topRecommond.urlType + '/' + topRecommond.linkId">
+            <p class="title">{{topRecommend.totle}}</p>
+            <div class="tags">
+              <iv-tag :color="tag.id | mapTagColor" v-for="(tag) in topRecommend.tagList" :key="tag.id">{{tag.name}}</iv-tag>
+            </div>
+          </a>
+        </div>
+      </div>
+    </panel>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Recommend"
+    }
+</script>
+
+<style scoped>
+
+</style>
