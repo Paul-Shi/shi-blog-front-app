d0a898c18962aa862e27630fde859378dae1f26d
shishunyuan <shishunyuan@iboxpay.com>
2020-07-20 21:36:35 +0800

    首页展示-8

--- src/components/content/ArticleListContent.vue
+++ src/components/content/ArticleListContent.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="article-list-content">
     <iv-row>
-      <iv-col :xs="24":sm="24" :md="24" :lg="17">
+      <iv-col :xs="24" :sm="24" :md="24" :lg="17">
         <div class="layout-left">
           <article-list-header v-if="categoryList.length>0" @filterByMenu="filterByMenu"
                                @filterByCategory="filterByCategory"
                                :categorys="categoryList"
                                :defaultCategory="selected_category"
-                               :mainTitle="文章列表" :sub-title="'Articles'"></article-list-header>
+                               :mainTitle="'文章列表'" :sub-title="'Articles'" ></article-list-header>
           <article-list-cell v-for="article in articleList" :article="article" :key="article.id"></article-list-cell>
-          <browse-more @browseMore="browseMore" :noMoreData="noMoreData" ref="browseMore"></browse-more>
+          <browse-more @browseMore="browseMore" :noMoreData="noMoreData"  ref="browseMore"></browse-more>
         </div>
       </iv-col>
       <iv-col :xs="0" :sm="0" :md="0" :lg="7">
@@ -32,7 +32,6 @@
   import BrowseMore from '@/components/views/BrowseMore'
   import merge from 'lodash/merge'
   import {treeDataTranslate} from '@/utils'
-
   export default {
     data () {
       return {
@@ -48,6 +47,10 @@
         noMoreData: false
       }
     },
+    created () {
+      this.listArticle()
+      this.listCategory()
+    },
     methods: {
       listArticle () {
         let params = {
@@ -62,7 +65,7 @@
           method: 'get'
         }).then(({data}) => {
           if (data && data.code === 200) {
-            if (data.page.totalPage <= data.page.currentPage) {
+            if (data.page.totalPage <= data.page.currPage) {
               this.noMoreData = true
             } else {
               this.noMoreData = false
@@ -94,7 +97,7 @@
         this.categoryId = params
         this.listArticle()
       },
-      resetLCurrentPage () {
+      resetCurrentPage () {
         this.currentPage = 1
       },
       browseMore () {
@@ -108,8 +111,57 @@
         this.$http({
           url: this.$http.adornUrl('/articles'),
           params: this.$http.adornParams(params),
+          method: 'get'
+        }).then(({data}) => {
+          if (data && data.code === 200) {
+            if (data.page.totalPage <= data.page.currPage) {
+              this.noMoreData = true
+            } else {
+              this.noMoreData = false
+            }
+            this.articleList = this.articleList.concat(data.page.list)
+          }
+        }).then(response => {
+          this.$refs.browseMore.stopLoading()
+        }).catch(error => {
+          this.$refs.browseMore.stopLoading()
+          console.log(error)
         })
       }
+    },
+    components: {
+      'article-list-header': ArticleListHeader,
+      'article-page-content': ArticlePageContent,
+      'article-page-footer': ArticlePageFooter,
+      'article-list-cell': ArticleListCell,
+      'recommend': Recommend,
+      'tag-wall': TagWall,
+      'browse-more': BrowseMore
     }
   }
 </script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .article-list-content
+    width auto
+    @media only screen and (max-width: 768px)
+      margin 5px 5px 10px 5px
+    @media screen and (min-width: 768px)
+      margin 10px 10px 20px 10px
+    @media screen and (min-width: 992px)
+      margin 15px 35px 50px 35px
+    @media screen and (min-width: 1200px)
+      width 1200px
+      margin 15px auto 0
+      margin-bottom 50px
+    .layout-left, .layout-right
+      padding 0
+      @media only screen and (max-width: 768px)
+        padding 0
+      @media screen and (min-width: 768px)
+        padding 0
+      @media screen and (min-width: 992px)
+        padding 0 10px
+      @media screen and (min-width: 1200px)
+        padding 0 10px
+</style>
